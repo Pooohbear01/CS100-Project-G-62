3f9db937524c386dedb5daec2d1e3f36072f8b27
btkkkkkkk <thanarat.boss12@gmail>
2023-12-11 04:30:58 +0700

    delete gender

--- Our CSTU Passport/Form/script.js
+++ Our CSTU Passport/Form/script.js
@@ -17,30 +17,6 @@ function validateName() {
   }
   return true;
 }
-/*
-// Event for input validation on user input
-document.getElementsByName('gender').addEventListener("inputGender")
-
-// Function to validate Gender
-function validateGender() {
-  var genderRadios = document.getElementsByName('gender');
-  var isCheck = false;
-  var errorElement = document.getElementById("genderError");
-  for(var i = 0 ; i < genderRadios.length ; i++) {
-    if(genderRadios[i].checked) {
-      isChecked = true;
-      break;
-    }
-  }
-  if(! isChecked) {
-    errorElement.textContent = "Please enter your gender.";
-    return false;
-  }else {
-    errorElement.textContent = ""; // Clear the error message when valid
-  }
-  return true;
-}
-*/
 
 // Function to validate Date of Birth
 function validateDateofBirth(){
